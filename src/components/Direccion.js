391f14239bfdec6512e0218b67ea5e384abb40b8
Lizeth Zuñiga <111620498+lizethzu98@users.noreply.github.com>
2023-01-14 22:23:05 -0600

    direccion

--- src/components/Direccion.js
+++ src/components/Direccion.js
@@ -7,18 +7,19 @@ const Direccion = () => {
     <img id="contact" src={require('../image/cafeteria.jpeg')} className="w3-image w3-greyscale" alt="Map" style={{width:"100%"}}/>
 
       <div className='w3-container w3-red w3-grayscale w3-xlarge'>
-        <h1><b>Opening Hours</b></h1>
+        <h1><b>APERTURA!</b></h1>
+        <h4><b>Nos ubicamos en calle Aldama S/N Tlalpan Ciudad de México</b></h4>
 
         <div className="w3-row">
           <div className="w3-half">
-            <p>Mon &amp; Tue CLOSED</p>
-            <p>Wednesday 10.00 - 24.00</p>
-            <p>Thursday 10:00 - 24:00</p>
+            <p>Abrimos &amp; Cerramos</p>
+            <p>Miercoles 10.00 - 24.00</p>
+            <p>Jueves 10:00 - 24:00</p>
           </div>
           <div className="w3-half">
-            <p>Friday 10:00 - 12:00</p>
-            <p>Saturday 10:00 - 23:00</p>
-            <p>Sunday Closed</p>
+            <p>Viernes 10:00 - 12:00</p>
+            <p>Sábado 10:00 - 23:00</p>
+            <p>Domingo Cerramos</p>
           </div>
 
         </div >
