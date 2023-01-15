7b45aba372dec84e324e0db5aae19ddcd7289256
Lizeth Zuñiga <111620498+lizethzu98@users.noreply.github.com>
2023-01-14 22:15:59 -0600

    menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -18,38 +18,14 @@ const Menu = () => {
           <h1><b>Espresso</b> <span class="w3-tag w3-red w3-round">Hot!</span>
             <span className="w3-right w3-tag w3-dark-grey w3-round">$100.00</span></h1>
           <p className="w3-text-grey">Tostado Arabica 100%</p>
-        </div>
 
-        <h1 id="pasta" className="w3-center w3-jumbo w3-padding-32">PASTA</h1>
-        <div className="w3-container w3-white w3-padding-32">
+          <h1><b>Café Mocha</b> <span class="w3-tag w3-red w3-round"></span>
+            <span className="w3-right w3-tag w3-dark-grey w3-round">$100.00</span></h1>
+          <p className="w3-text-grey">Espresso, crema, leche, espuma</p>
 
-          <h1><b>Lasagna</b> <span className="w3-tag w3-grey w3-round">Popular</span>
-            <span className="w3-right w3-tag w3-dark-grey w3-round">$13.50</span></h1>
-          <p className="w3-text-grey">Special sauce, mozzarella, parmesan, ground beef</p>
-          <hr />
-
-          <h1><b>Ravioli</b> <span class="w3-right w3-tag w3-dark-grey w3-round">$14.50</span></h1>
-          <p className="w3-text-grey">Ravioli filled with cheese</p>
-          <hr />
-
-          <h1><b>Spaghetti Classica</b> <span className="w3-right w3-tag w3-dark-grey w3-round">$11.00</span></h1>
-          <p className="w3-text-grey">Fresh tomatoes, onions, ground beef</p>
-        </div>
-
-        <h1 id="starters" className="w3-center w3-jumbo w3-padding-32">STARTERS</h1>
-        <div className="w3-container w3-white w3-padding-32">
-
-          <h1><b>Today's Soup</b> <span className="w3-tag w3-grey w3-round">Seasonal</span>
-            <span className="w3-right w3-tag w3-dark-grey w3-round">$5.50</span></h1>
-          <p className="w3-text-grey">Ask the waiter</p>
-          <hr />
-
-          <h1><b>Bruschetta</b> <span className="w3-right w3-tag w3-dark-grey w3-round">$8.50</span></h1>
-          <p className="w3-text-grey">Bread with pesto, tomatoes, onion, garlic</p>
-          <hr />
-
-          <h1><b>Garlic bread</b> <span className="w3-right w3-tag w3-dark-grey w3-round">$9.50</span></h1>
-          <p className="w3-text-grey">Grilled ciabatta, garlic butter, onions</p>
+          <h1><b>Café Chai</b> <span class="w3-tag w3-red w3-round"></span>
+            <span className="w3-right w3-tag w3-dark-grey w3-round">$80.00</span></h1>
+          <p className="w3-text-grey">Té de nuez y especias</p>
         </div>
       </div>
     </div>
