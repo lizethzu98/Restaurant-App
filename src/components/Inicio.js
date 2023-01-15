f9157b5a175c83b38ef9f7a327d8f5a61a664ff8
Lizeth Zuñiga <111620498+lizethzu98@users.noreply.github.com>
2023-01-14 18:39:34 -0600

    inicio

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -1,24 +1,21 @@
 import React from 'react'
 import '../w3.css';
 import '../font.css';
-import { Helmet } from "react-helmet";
-import '../scss/BarraMenu.scss';
 
 
 const Inicio = () => {
   return (
-    <div>
-
+    
       <div className="w3-display-container w3-grayscale-min imagenInicio">
         <div className="w3-display-bottomleft">
-          <span className="w3-tag w3-xlarge">Open from 10am to 12pm</span>
+          <span className="w3-tag w3-xlarge">Abrimos de 10am a 12pm</span>
         </div>
         <div className="w3-display-middle w3-center">
-          <span className="w3-text-white">thin<br />CRUST PIZZA</span>
-          <p><a href="#menu" className="w3-button w3-xxlarge w3-black">Let me see the menu</a></p>
+          <span className="w3-text-white">MI<br />CAFÉ</span>
+          <p><a href="/menu" className="w3-button w3-xxlarge w3-black">Ir al Menú</a></p>
         </div>
       </div>
-    </div>
+    
   )
 }
 
